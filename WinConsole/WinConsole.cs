d40570f4ffc9df532a68c4dbe208d9782cd199f2
JavaCommons Technologies <javacommons@gmail.com>
2024-06-03 13:18:00 +0900

    WinConsole-v2024.0603.1317.52

--- WinConsole/WinConsole.cs
+++ WinConsole/WinConsole.cs
@@ -2,6 +2,7 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System;
 using System.Text;
+using Windows.Win32;
 namespace Global;
 public static class WinConsole
 {
@@ -9,8 +10,8 @@ public static class WinConsole
     public static void Alloc()
     {
         if (Environment.OSVersion.Platform != PlatformID.Win32NT) return;
-        Encoding encoding = CodePagesEncodingProvider.Instance.GetEncoding((int)GetACP());
-        AllocConsole();
+        Encoding encoding = CodePagesEncodingProvider.Instance.GetEncoding((int)PInvoke.GetACP());
+        PInvoke.AllocConsole();
         var stdout = new StreamWriter(Console.OpenStandardOutput(), encoding);
         stdout.AutoFlush = true;
         Console.SetOut(stdout);
@@ -25,26 +26,6 @@ public static class WinConsole
         if (Environment.OSVersion.Platform != PlatformID.Win32NT) return;
         Console.SetOut(TextWriter.Null);
         Console.SetError(TextWriter.Null);
-        FreeConsole();
+        PInvoke.FreeConsole();
     }
-    #region Win API Functions and Constants
-    [DllImport("kernel32.dll",
-               EntryPoint = "AllocConsole",
-               SetLastError = true,
-               CharSet = CharSet.Auto,
-               CallingConvention = CallingConvention.StdCall)]
-    private static extern int AllocConsole();
-    [DllImport("kernel32.dll",
-               EntryPoint = "AttachConsole",
-               SetLastError = true,
-               CharSet = CharSet.Auto,
-               CallingConvention = CallingConvention.StdCall)]
-    private static extern UInt32 AttachConsole(UInt32 dwProcessId);
-    [DllImport("kernel32.dll")]
-    private static extern bool FreeConsole();
-    private const UInt32 ERROR_ACCESS_DENIED = 5;
-    private const UInt32 ATTACH_PARRENT = 0xFFFFFFFF;
-    [DllImport("kernel32.dll")]
-    private static extern uint GetACP();
-    #endregion
 }
